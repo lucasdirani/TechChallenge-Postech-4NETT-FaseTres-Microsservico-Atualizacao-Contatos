64169bec9ab61a3e8d02f4241ce80b6ec2769e98
Lucas Dirani <lruizd@protonmail.com>
2024-08-21 15:10:54 -0300

    feat(rabbitmq): incluindo classe para leitura de configurações de conexão com o serviço de mensageria RabbitMQ.

---
+++ src/Postech.GroupEight.TechChallenge.ContactUpdate.Infra/Messaging/RabbitMQ/Configurations/RabbitMQConnectionConfiguration.cs
@@ -0,0 +1,29 @@
+using System.Diagnostics.CodeAnalysis;
+
+namespace Postech.GroupEight.TechChallenge.ContactUpdate.Infra.Messaging.RabbitMQ.Configurations
+{
+    [ExcludeFromCodeCoverage]
+    public record RabbitMQConnectionConfiguration
+    {
+        public required string HostAddress { get; init; }
+        public required string HostUsername { get; init; }
+        public required string HostPassword { get; init; }
+        public required RabbitMQCircuitBreakerConfiguration CircuitBreakerConfiguration { get; init; }
+        public required IEnumerable<RabbitMQEndpointConfiguration> Endpoints { get; init; }
+    }
+
+    [ExcludeFromCodeCoverage]
+    public record RabbitMQEndpointConfiguration
+    {
+        public required string EndpointName { get; init; }
+    }
+
+    [ExcludeFromCodeCoverage]
+    public record RabbitMQCircuitBreakerConfiguration
+    {
+        public int TrackingPeriodInMinutes { get; init; }
+        public double TripThreshold { get; init; }
+        public int ActiveThreshold { get; init; }
+        public int ResetIntervalInMinutes { get; init; }
+    }
+}
